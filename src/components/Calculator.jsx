ab403438c19bc1404f79c07d150b93d50eaf9f1b
Joao Beier <joao.beier@outlook.com>
2026-07-20 00:47:38 -0400

    the validCheck boolean branch needs fixing desperately!

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -7,10 +7,11 @@ function Calculator () {
 
     const {type, typeSetter} = useNavContext()
     const [equation, setEquation] = useState("0")
-    const handleTypedContent = (val) => {
+    const handleTypedContent = (realVal) => {
         let validCheck = false
         let numCheck = true
-
+        const val = realVal.replaceAll(" ", "")
+        console.log("val:" + val )
         //function to check if a given character is a number
         const isNum = (x) => {
            const nums = x === "0" || x === "1" || x === '2' || x === '3' || x === '4' || x === '5' || x === '6' || x === '7' || x === '8' || x === '9'
@@ -24,7 +25,6 @@ function Calculator () {
                 }
            }
         }
-
         //loop to check whether or not the value passed contains any non numerical characters
         for (const c of val){
             if (!isNum(c)) {
@@ -32,21 +32,32 @@ function Calculator () {
                 break
             }
         }
-
         //basic type checker
         if (type === "basic"){ //!!!!come bac here to deal with negatives!!!!!
             if ((val >= '0' && val <= '9') || (val === '/' || val === '*' || val === '+' || val === '-')){
-                validCheck = true
+                validCheck = true //THIS NEEDS FIXING!!!!!
             }
-
             if (validCheck && numCheck){
-                if (equation === "0"){
+                if (equation === "0"){ //equation here is the previous one before being updated with the current input
+                    // for (const c of val){
+                    //     if (c != "0") cleanedVal += c //cleans leading 0s to make input looking clean
+                    // }
+                    //the for loop might not be needed, read down below to figure out why.
+                    /*
+                        this handTypedContent function is called when a character is typed. and every time the entire input box is
+                        passed to it. So this equation === '0' check literally only runs once. and it runs only when there are 2 
+                        characters. it gets a lil fuzzy if you enter 0 as your second character, but we can handle that later
+                    */
+
+                    const lastVal = val[1]
                     let cleanedVal = ""
-                    for (const c of val){
-                        if (c != "0") cleanedVal += c
+                    if (lastVal === '+' || lastVal === '-' || lastVal === '/' || lastVal === '*') {
+                        cleanedVal = " " + lastVal + " "
+                    } else {
+                        cleanedVal = lastVal
                     }
                     setEquation(cleanedVal)
-                } else {
+                }else {
                     setEquation(val)
                 }
                 console.log(equation)
@@ -60,12 +71,28 @@ function Calculator () {
             }
         }
     }
+
+    const calculateResult = () => {
+        /* We have to parse the equation var here and break it into a list, to make it easier to access things
+            1. we have to make sure that the string being passed through has some blank space between stuff already to make it easier to parse
+
+
+        */
+    }
+    
     return (
         <>
             <div className="calc-content">
                 {/*the input screen will go right here */}
                 <div className="calc-input-screen">
-                    <input type = "text" value = {equation} onChange = {(e) => handleTypedContent(e.target.value)}></input>
+                    <input type = "text" 
+                           value = {equation} 
+                           onChange = {(e) => handleTypedContent(e.target.value)}
+                           onKeyDown={ (e) => {
+                            if (e.key === 'Enter') {
+                                calculateResult()
+                            }
+                           }}></input>
                 </div>
                 <div className="calc-board">
 
